ce343cf2b3d791cc0a55ed42204b10194dd831f5
turnwol7 <justinb.developer@gmail.com>
2025-01-22 14:08:38 -0400

    refactor main to add Config struct

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,27 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     // dbg!(args);
 
-    // get args
-    let query = &args[1];
-    let file_path = &args[2];
-
-    println!("Searching for {query}");
-    println!("In File {file_path}");
-
     // read from file
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let config = parse_config(&args);
+    
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    // get args
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
+
 }
