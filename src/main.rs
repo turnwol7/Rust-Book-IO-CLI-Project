1cf89d6f49385ac909467d1f67da6c7ac0c562b0
turnwol7 <justinb.developer@gmail.com>
2025-01-22 13:52:47 -0400

    add read file operation to text file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-   // dbg!(args);
+    // dbg!(args);
 
+    // get args
     let query = &args[1];
     let file_path = &args[2];
 
     println!("Searching for {query}");
     println!("In File {file_path}");
+
+    // read from file
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
