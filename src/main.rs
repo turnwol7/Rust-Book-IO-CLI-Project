22823d7e9e0adcc41b2b28f2615542f73edf982f
turnwol7 <justinb.developer@gmail.com>
2025-01-22 15:29:17 -0400

    separate lib and main functionality

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use core::panic;
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use minigrep::Config;
+use minigrep::run;
 
 
 fn main() {
@@ -17,42 +17,11 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
 
 // common struct for inputs
-struct Config {
-    query: String,
-    file_path: String,
-}
 
-impl Config {
-        fn build(args: &[String]) -> Result<Config, &'static str> {
-            if args.len() < 3 {
-                return Err("not enough arguments");
-            }
-    
-            let query = args[1].clone();
-            let file_path = args[2].clone();
-    
-            Ok(Config { query, file_path })
-        }
-    }
-
-
-fn parse_config(args: &[String]) -> Config {
-    // get args
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Config { query, file_path }
-
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
