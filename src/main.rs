bc1eae14df5db3c44263a60b7c1ce9797ec091f5
turnwol7 <justinb.developer@gmail.com>
2025-01-23 11:25:01 -0400

    added functional closures to shorten code.

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,11 @@ use minigrep::Config;
 // A mini grep search CLI to search a text file and return the lines that the query term was included in.
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    //let args: Vec<String> = env::args().collect();
 
     // read from file
     // print output from std out to text -> cargo run -- to poem.txt > output.txt
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
