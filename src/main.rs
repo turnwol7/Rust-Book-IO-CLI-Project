4fb4204f299800644cf62914a4091698f462041f
turnwol7 <justinb.developer@gmail.com>
2025-01-22 20:43:56 -0400

    format std out to print to file

--- src/main.rs
+++ src/main.rs
@@ -10,15 +10,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     // read from file
+    // print output from std out to text -> cargo run -- to poem.txt > output.txt
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
 
     if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
